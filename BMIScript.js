fceefbb14133c763fa57baef4b00d11445f9d6fc
Lynjah <masterlyn21@gmail.com>
2020-07-30 08:05:28 -0600

    Added Azure deployment

---
+++ BMIScript.js
@@ -0,0 +1,6 @@
+﻿function bmi() {
+    var height = Number((document.getElementById("height").value) * (document.getElementById("height").value));
+    var weight = Number((document.getElementById("weight").value) * 703);
+    var result = weight / (height);
+    document.getElementById("result").innerHTML = "Your bmi score is : " + result;
+}
